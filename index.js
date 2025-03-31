c5260c01ab9f3bb0c0f73bd474eb679d928fb6a8
Mathias Munene <munenemathias@gmail.com>
2025-03-31 15:36:48 +0300

    final

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Create a function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  }
+  
+  // 2. Create an arrow function called square
+  const square = x => x * x;
+  
+  // 3. Create an arrow function called add
+  const add = (a, b) => a + b;
